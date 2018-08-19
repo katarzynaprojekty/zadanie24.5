c4fe505c53cd116ce6e5aa85132b6aaa034ca798
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-19 17:34:22 +0200

    Add calculating points for style

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,6 +1,10 @@
 // calculateStylePoints.js
 const calculateStylePoints = (styleNotes) => {
 
+    let stylePointsOrdered = styleNotes.sort( );
+
+    return stylePointsOrdered[1] + stylePointsOrdered[2] + stylePointsOrdered[3];
+
 };
   
 module.exports = calculateStylePoints;
