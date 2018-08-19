a78df6753553ef941762168631d83e828802ae87
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-19 16:59:31 +0200

    Add calculating points for distance

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,6 +1,18 @@
 // calculateDistancePoints.js
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
-  
+    let distancePoints;
+
+    if (hillSize == 'normal' ){ //Skocznia normalna w Pjongjangu
+        distancePoints = 60 + (2 * (distance - kPoint));
+    } 
+    else if (hillSize == 'big') { //Skocznia duża w Zakopanem
+        distancePoints = 60 + (1.8 * (distance - kPoint));
+    } 
+    else if (hillSize == 'mammoth') { //Skocznia mamucia w Planicy
+        distancePoints = 120 + (1.2 * (distance - kPoint));
+    }
+
+    return distancePoints;
 };
- module.exports = calculateDistancePoints;
- 
+
+module.exports = calculateDistancePoints;
